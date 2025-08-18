84641d334ed5480643cc7064e6277fc82c67e8d3
Joshua A VanLaere <116771383+Jvanl2000@users.noreply.github.com>
2025-08-18 14:01:40 +0000

    Section 1.1

---
+++ Section 1.1/section.java
@@ -0,0 +1,18 @@
+public class section {
+    public static void main(String[] args) {
+        HelloWorld();
+    }
+
+    static void HelloWorld() { 
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+    }
+}
