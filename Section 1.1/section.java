2dbb50260d1743bc61d59a367ff17a1c09fa4ccb
Joshua A VanLaere <116771383+Jvanl2000@users.noreply.github.com>
2025-08-19 14:05:07 -0400

    Update section.java

--- Section 1.1/section.java
+++ Section 1.1/section.java
@@ -1,7 +1,14 @@
 public class section {
     public static void main(String[] args) {
         // HelloWorld();
-        UseArgument(args);
+        // UseArgument(args);
+        UseThree(args);
+    }
+
+    static void UseThree(String[] args) {
+        System.out.print(args[2] + ", ");
+        System.out.print(args[1] + ", and ");
+        System.out.println(args[0] + " are learning Java!");
     }
 
     static void UseArgument(String[] args) {
