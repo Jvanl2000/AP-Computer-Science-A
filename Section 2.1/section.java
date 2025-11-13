1a9821a9d69633c74c6d68851a25193e60989893
TacitCoding <ghickswent@gmail.com>
2025-11-13 15:15:45 +0000

    2.1.1 and 2.1.2 done

--- Section 2.1/section.java
+++ Section 2.1/section.java
@@ -1,5 +1,41 @@
 public class section {
   public static void main(String[] args) {
-    System.out.printlnt("Hello, World!");
+    //StdOut.println(max3(3.0, 2.0, 5.0));
+    StdOut.println(odd(true, true, true));
   }
+
+//2.1.1
+
+  public static int max3(int a, int b, int c) {
+    int max = a;
+    if (b > max)
+      max = a;
+    if (c > max)
+      max = c;
+    return max;
+  }
+
+  public static double max3(double a, double b, double c) {
+    double max = a;
+    if (b > max)
+      max = a;
+    if (c > max)
+      max = c;
+    return max;
+  }
+
+//2.1.2
+
+  public static boolean odd(boolean a, boolean b, boolean c) {
+    boolean[] trueCount = new boolean[] {a, b, c};
+    int sum = 0;
+    for (boolean truer : trueCount)
+      if (truer)
+        sum++;
+    if (sum % 2 == 0)
+      return false;
+    return true;
+  }
+
+
 }
