349980761f523567aa02de2adaa765852d64c295
Joshua A VanLaere <116771383+Jvanl2000@users.noreply.github.com>
2026-01-06 02:34:58 +0000

    Section 2.3
    Co-authored-by: TacitCoding <TacitCoding@users.noreply.github.com>

--- Section 2.3/section.java
+++ Section 2.3/section.java
@@ -1,1 +1,21 @@
+public class section {
+    public static void main(String[] args) {
+        intToBinary(11);
+    }
 
+    public static void intToBinary(int n) {
+        String temp = "";
+
+        while (n > 0) {
+            temp += String.valueOf(n%2);
+            n = n/2;
+        }
+
+        System.out.println(reverse(temp));
+    }
+
+    public static String reverse(String string) {
+        if (string.length() <= 1) return string;
+         else return string.charAt(string.length() - 1) + reverse(string.substring(0, string.length() - 1));
+    }
+}
